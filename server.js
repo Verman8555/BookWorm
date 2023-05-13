c30b39b7f65bf04bfa6bdfa0b1473bf1134bd1e4
Verman8555 <83014894+Verman8555@users.noreply.github.com>
2023-05-13 21:04:11 +0530

    trail establishing routes

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
+const methodOverride = require('method-override');
 const WriterSchema = require('./models/structure');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+//app.use(methodOverride('_method'));
 
 //dotenv.config({path:'config.env'});
 mongoose.connect('mongodb+srv://vermanvishal8555:nT9eVnwYGfY3pw0N@cluster0.fatjnzh.mongodb.net/', {
@@ -17,6 +23,7 @@ mongoose.connect('mongodb+srv://vermanvishal8555:nT9eVnwYGfY3pw0N@cluster0.fatjn
 /* mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true}, () => 
     console.log('Database Connected')
 ); */
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -25,6 +32,17 @@ db.once("open", () => {
 
 app.get('/',(req,res) => {
     res.send("hi");
+})
+
+app.post('/post', async (req,res) => {
+    const data = new WriterSchema({
+        Name:"nmsss",
+        Century: "cskdj",
+        Genre: "jhbjadch",
+        Highlight: "jhbdjs"
+    });
+    await data.save();
+    res.send(data);
 })
 
 app.listen(1234,() => {
